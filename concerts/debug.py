8038d05e86a35974ddb671a05ffb6f9f1a0e5b66
IanDanCheruiyot <daniank08@gmail.com>
2024-09-27 17:02:53 +0300

    inittables

---
+++ concerts/debug.py
@@ -0,0 +1,113 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import ipdb
+from models import Band, Venue, Concert
+
+if __name__ == '__main__':
+    
+    engine = create_engine('sqlite:///cocerts.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    ipdb.set_trace()  
+
+    # Test retrieving the first Band
+    first_band = session.query(Band).first()
+    if first_band:
+        print("First Band:")
+        print(" Name:", first_band.name)
+        print(" Hometown:", first_band.hometown)
+
+        # Band concerts()
+        # Returns a collection of all the concerts that the Band has played
+        print(" Concerts for First Band:", [concert.name for concert in first_band.concerts()])
+
+        # Band venues()
+        # Returns a collection of all the venues that the Band has performed at
+        print(" Venues for First Band:", [venue.title for venue in first_band.venues()])
+
+        # Band play_in_venue(venue, date)
+        # Creates a new concert for the band in the specified venue on the given date
+        if session.query(Venue).count() > 0:  # Ensuring there are venues available
+            venue = session.query(Venue).first()  # Getting the first venue
+            new_concert = first_band.play_in_venue(venue, "2024-10-11")
+            session.add(new_concert)
+            session.commit()
+            print("A new concert has been created Successfully:")
+            print("Name:", new_concert.name)
+            print("Date:", new_concert.date)
+        
+        # Band all_introductions()
+        # Returns an array of strings representing all the introductions for this band
+        example_introduction = print("Introductions for First Band:", first_band.all_introductions())
+
+        # Band most_performances()
+        # Returns the Band instance for the band that has played the most concerts
+        most_performed_band = Band.most_performances(session)
+        print("\nBand with the most performances:")
+        print("Name:", most_performed_band.name)
+        print("Hometown:", most_performed_band.hometown)
+        print("Total Concerts:", len(most_performed_band.concerts()))  
+
+
+
+
+    # Test retrieving the first Venue
+    first_venue = session.query(Venue).first()
+    if first_venue:
+        print("\nFirst Venue:")
+        print(" Title:", first_venue.title)
+        print(" City:", first_venue.city)
+
+        # Venue concerts()
+        # Returns a collection of all the concerts for the Venue
+        print(" Concerts for First Venue:", [concert.name for concert in first_venue.concerts()])
+
+        # Venue bands()
+        # Returns a collection of all the bands who performed at the Venue
+        print(" Bands for First Venue:", [band.name for band in first_venue.bands()])
+
+        # Venue concert_on(date)
+        # Finds and returns the first concert on that date at that venue
+        concert_date = "2024-10-01"  
+        concert = first_venue.concert_on(concert_date)
+        if concert:
+            print(f"\nConcert on {concert_date} at {first_venue.title}:")
+            print("Name:", concert.name)
+            print("Date:", concert.date)
+        else:
+            print(f"No concert found on {concert_date} at {first_venue.title}.")
+
+        # Venue most_frequent_band()
+        # Returns the band with the most concerts at the venue
+        the_most_frequent_band, concert_counts = first_venue.most_frequent_band()
+        if the_most_frequent_band:
+            print(f"\nMost Frequent Band at {first_venue.title}:")
+            print("Name:", the_most_frequent_band.name)
+            print("Hometown:", the_most_frequent_band.hometown)
+            print("Number of Concerts:", concert_counts)
+        else:
+            print(f"No concerts found at {first_venue.title}.")
+
+
+    # Test retrieving the first Concert
+    first_concert = session.query(Concert).first()
+    if first_concert:
+        print("\nFirst Concert:")
+        print(" Name:", first_concert.name)
+        print(" Date:", first_concert.date)
+
+        # Returns the Band instance for this Concert
+        print(" Band for First Concert:", first_concert.band().name)
+
+        
+        # Returns the Venue instance for this Concert
+        print(" Venue for First Concert:", first_concert.venue().title)
+
+        
+        # Returns True if the concert is in the band's hometown, False if it is not
+        print(first_concert.hometown_show())
+
+        
+        # Returns a string with the band's introduction for this concert
+        print(first_concert.introduction())
---
+++ concerts/models.py
@@ -0,0 +1,134 @@
+import os
+from sqlalchemy.orm import sessionmaker, relationship, backref
+import sys
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, PrimaryKeyConstraint
+from sqlalchemy import func
+from sqlalchemy.ext.declarative import declarative_base
+sys.path.append(os.getcwd)
+
+# Create an SQLite database engine and initialize the database schema
+engine = create_engine('sqlite:///concerts.db', echo=True)
+# Create a session factory bound to the engine
+Session = sessionmaker(bind=engine)
+session = Session()
+# Initialize the Base for the models
+Base = declarative_base()
+
+# Band Model
+class Band(Base):
+    __tablename__ = 'bands'
+    id = Column(Integer() , primary_key = True)
+    name = Column(String(255) , nullable = False)
+    hometown = Column(String(255) , nullable = False)
+    # Defining the relationship
+    concerts_list = relationship(
+        'Concert', 
+        back_populates = 'band_instance'
+    )
+
+    # Object Relationship Methods
+
+    def concerts(self):
+        return self.concerts_list
+    
+    def venues(self):
+        return [concert.venue_instance for concert in self.concerts_list]
+    
+    # Aggregate and Relationship Methods
+    
+    def play_in_venue(self, venue, date):
+        concert_name = f"{self.name} Festival"
+        new_concert = Concert(name=concert_name, date=date, band_instance=self, venue_instance=venue)
+        return new_concert
+    
+    def all_introductions(self):
+        introductions = []
+        for concert in self.concerts(): 
+            introductions.append(concert.introduction())
+        return introductions
+    
+    @classmethod
+    def most_performances(cls, session):
+        count = {}
+        bands = session.query(cls).all()
+        for band in bands:
+            count[band] = len(band.concerts())  
+        most_performed_band = max(count, key=count.get)
+        return most_performed_band
+
+
+# Venue Model
+class Venue(Base):
+    __tablename__ = 'venues'
+    id = Column(Integer() , primary_key = True)
+    title = Column(String(255) , nullable = False)
+    city = Column(String(255) , nullable = False)
+    # Defining the relationship
+    concerts_list = relationship(
+        'Concert',
+        back_populates = 'venue_instance'
+    )
+
+    # Object Relationship Methods
+
+    def concerts(self):
+        return self.concerts_list
+    
+    def bands(self):
+        return [concert.band_instance for concert in self.concerts_list] 
+    
+    # Aggregate and Relationship Methods
+
+    def concert_on(self, date):
+        for concert in self.concerts():  
+            if concert.date == date:
+                return concert
+        return None  # If there is no concert on that specific date
+    
+    def most_frequent_band(self):
+        concert_counts = {}
+        
+        for concert in self.concerts():  
+            band = concert.band_instance  
+            if band in concert_counts:
+                concert_counts[band] += 1
+            else:
+                concert_counts[band] = 1
+        
+        most_frequent = None
+        max_count = 0
+        
+        for band, count in concert_counts.items():
+            if count > max_count:
+                most_frequent = band
+                max_count = count
+        
+        return most_frequent, max_count
+
+
+# Concert Model
+class Concert(Base):
+    __tablename__ = 'concerts'
+    id = Column(Integer() , primary_key = True)
+    name = Column(String(255) , nullable = False)
+    date = Column(String(50) , nullable = False)
+    band_id = Column(Integer, ForeignKey('bands.id'))
+    venue_id = Column(Integer, ForeignKey('venues.id'))
+    band_instance = relationship('Band', back_populates='concerts_list')
+    venue_instance = relationship('Venue', back_populates='concerts_list')
+
+    # Object Relationship Methods
+
+    def band(self):
+        return self.band_instance
+    
+    def venue(self):
+        return self.venue_instance
+    
+    # Aggregate and Relationship Methods
+
+    def hometown_show(self):
+        return self.venue_instance.city == self.band_instance.hometown
+    
+    def introduction(self):
+        return f"Hello {self.venue_instance.city}!!!!! We are {self.band_instance.name} and we're from {self.band_instance.hometown}"
